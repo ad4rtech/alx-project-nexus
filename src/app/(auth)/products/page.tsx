53ec7855044d536ebfe918f3c25f1d8bd21a2bc5
Fidel Adar <adarfidel.ogw@gmail.com>
2026-02-07 00:56:07 +0300

    fix: modified tailwind attributes

--- src/app/(auth)/products/page.tsx
+++ src/app/(auth)/products/page.tsx
@@ -45,7 +45,7 @@ export default function ProductsPage() {
 
             {/* Toolbar */}
             <div className="flex flex-col sm:flex-row gap-4 justify-between items-center">
-                <div className="relative flex-grow max-w-md w-full">
+                <div className="relative grow max-w-md w-full">
                     <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                         <Search className="h-4 w-4 text-gray-400" />
                     </div>
